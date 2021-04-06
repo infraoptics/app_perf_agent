f314a7665a33f6f433ad000254435492a98e4f0d
Chris Roberts <chrobert@redhat.com>
2021-04-06 17:35:15 -0400

    Add some more metrics and doc links to vmstat vars

--- lib/app_perf_agent/plugin/system/memory.rb
+++ lib/app_perf_agent/plugin/system/memory.rb
@@ -8,14 +8,20 @@ module AppPerfAgent
     module System
       # Memory class to collect memory stats from the host
       class Memory < AppPerfAgent::Plugin::Base
+        # https://github.com/threez/ruby-vmstat/blob/master/lib/vmstat/memory.rb#L2-L16
+        # https://github.com/threez/ruby-vmstat/blob/master/lib/vmstat/linux_memory.rb#L7-L8
         def call
           memory = Vmstat.memory
           [
-            ['system.memory.free_bytes',     memory.free_bytes],
-            ['system.memory.inactive_bytes', memory.inactive_bytes],
-            ['system.memory.active_bytes',   memory.active_bytes],
-            ['system.memory.wired_bytes',    memory.wired_bytes],
-            ['system.memory.total_bytes',    memory.total_bytes]
+            ['system.memory.free_bytes',       memory.free_bytes],
+            ['system.memory.inactive_bytes',   memory.inactive_bytes],
+            ['system.memory.active_bytes',     memory.active_bytes],
+            ['system.memory.wired_bytes',      memory.wired_bytes],
+            ['system.memory.total_bytes',      memory.total_bytes],
+            ['system.memory.available_bytes',  memory.available_bytes],
+            ['system.memory.pagesize',         memory.pagesize],
+            ['system.memory.pageins',          memory.pageins],
+            ['system.memory.pageouts',         memory.pageouts]
           ]
         end
       end
--- lib/app_perf_agent/plugin/system/network.rb
+++ lib/app_perf_agent/plugin/system/network.rb
@@ -8,12 +8,16 @@ module AppPerfAgent
     module System
       # Network class to collect network stats from the host
       class Network < AppPerfAgent::Plugin::Base
+        # https://github.com/threez/ruby-vmstat/blob/master/lib/vmstat/network_interface.rb#L2-L8
         def call
           inets = Vmstat.network_interfaces
           inets.flat_map do |inet|
             [
               ['system.network.in_bytes', inet.in_bytes, { 'name' => inet.name.to_s }],
-              ['system.network.out_bytes', inet.out_bytes, { 'name' => inet.name.to_s }]
+              ['system.network.in_errors', inet.in_errors, { 'name' => inet.name.to_s }],
+              ['system.network.in_drops', inet.in_drops, { 'name' => inet.name.to_s }],
+              ['system.network.out_bytes', inet.out_bytes, { 'name' => inet.name.to_s }],
+              ['system.network.out_errors', inet.out_errors, { 'name' => inet.name.to_s }]
             ]
           end
         end
